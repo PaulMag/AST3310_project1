90665741200c7174c73314afb29e1ded550b646e
PaulMag <paulms@ulrik.uio.no>
2014-03-10 18:41:25 +0100

    The plotting file which plots everything into one figure.

---
+++ plot.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: python plot.py name_of_datafile"
+    print "Datafile is assumed to be in folder 'data/' and have the extension '.dat'."
+    sys.exit(1)
+
+infile = open("data/" + sys.argv[1] + ".dat", "r")
+
+info = infile.readline() # the first line only contains potential extra info
+
+# If you don't want to plot every step:
+#resolution = 0.003
+#skip = int( round( resolution / (time / n) ) )
+skip = 1
+
+dm  = []
+M   = []
+rho = []
+R   = []
+P   = []
+L   = []
+T   = []
+eps = []
+kap = []
+
+for line in infile:
+    line = line.split()
+
+    dm.append(float(line[0]))
+    M.append(float(line[1]))
+    rho.append(float(line[2]))
+    R.append(float(line[3]))
+    P.append(float(line[4]))
+    L.append(float(line[5]))
+    T.append(float(line[6]))
+    eps.append(float(line[7]))
+    kap.append(float(line[8]))
+
+infile.close()
+
+rho0 = rho[1]
+P0   = P[1]
+
+dm  = - np.array(dm)/ 1e21   # [1e21 kg]
+M   = np.array(M)   / M[0]   # [M0]
+rho = np.array(rho) / rho[0] # [rho0]
+R   = np.array(R)   / R[0]   # [R0]
+P   = np.array(P)   / (1e3*P[0]) # [1e3 P0]
+L   = np.array(L)   / L[0]   # [L0]
+T   = np.array(T)   / 1e7    # [1ey K]
+eps = np.array(eps)          # [W/kg]
+kap = np.array(kap) / 1e2    # [1e3 cm^2/g]
+
+plt.figure()
+
+#plt.title(info)
+plt.title("rho0 = %g kg/m^3 ,  P0 = %G Pa" % (rho0, P0), fontsize=18)
+plt.xlabel("mass [M0]")
+plt.grid('on')
+
+plt.hold("on")
+plt.plot(M[::skip], rho[::skip])
+plt.plot(M[::skip], R[::skip])
+plt.plot(M[::skip], P[::skip])
+plt.plot(M[::skip], L[::skip])
+plt.plot(M[::skip], T[::skip])
+plt.plot(M[::skip], eps[::skip])
+plt.plot(M[::skip], kap[::skip])
+#plt.plot(M[::skip], dm[::skip])
+
+plt.legend(["rho [rho0]", "R [R0]", "P [1e3 P0]", "L [L0]", "T [1e7 K]", \
+            "eps [W/kg]", "kappa 1e3 cm^2/g]"], loc="best")
+
+plt.show()
+

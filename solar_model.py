903084b67c5429d9f3e139b757b176d099e34160
PaulMag <paulms@ulrik.uio.no>
2014-05-19 00:27:36 +0200

    Used safer method to find the real root of xi. Langtangen taught us not to compare floats with '=='.

--- solar_model.py
+++ solar_model.py
@@ -355,7 +355,8 @@ while True:
         K = 4 * RR
         
         xi = np.roots([1./RR, 1, K, nabla_ad-nabla_rad]) # 2 complex and 1 real
-        xi = float( np.real( xi[np.imag(xi) == 0] ) ) # keeps the real root only
+        xi = float( np.real( xi[np.imag(xi) == np.min(np.imag(xi))] ) )
+            # keeps the real root only
         
         nabla = xi*xi + K * xi + nabla_ad
         

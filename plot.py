98a4abc7a3843db1d9b887bb5e32865ebfe69db8
PaulMag <paulms@ulrik.uio.no>
2014-05-19 00:36:57 +0200

    Added fluxplotting. Plots in 3 figures.

--- plot.py
+++ plot.py
@@ -25,6 +25,8 @@ L   = []
 T   = []
 eps = []
 kap = []
+F_r = []
+F_c = []
 
 for line in infile:
     line = line.split()
@@ -38,13 +40,15 @@ for line in infile:
     T.append(float(line[6]))
     eps.append(float(line[7]))
     kap.append(float(line[8]))
+    F_r.append(float(line[9]))
+    F_c.append(float(line[10]))
 
 infile.close()
 
 rho0 = rho[1]
 P0   = P[1]
 
-dm  = - np.array(dm)/ 1e21   # [1e21 kg]
+dm  = - np.array(dm)         # [kg]
 M   = np.array(M)   / M[0]   # [M0]
 rho = np.array(rho) / rho[0] # [rho0]
 R   = np.array(R)   / R[0]   # [R0]
@@ -53,15 +57,19 @@ L   = np.array(L)   / L[0]   # [L0]
 T   = np.array(T)   / 1e7    # [1ey K]
 eps = np.array(eps)          # [W/kg]
 kap = np.array(kap) / 1e2    # [1e3 cm^2/g]
+F_r = np.array(F_r)
+F_c = np.array(F_c)
 
-plt.figure()
 
-#plt.title(info)
-plt.title("rho0 = %g kg/m^3 ,  P0 = %G Pa" % (rho0, P0), fontsize=18)
-plt.xlabel("mass [M0]")
+
+
+# ************************************************************ #
+plt.figure(); plt.hold("on")
+
 plt.grid('on')
+plt.xlabel("mass [M0]")
+#plt.title("rho0 = %g kg/m^3 ,  P0 = %G Pa" % (rho0, P0), fontsize=18)
 
-plt.hold("on")
 plt.plot(M[::skip], rho[::skip])
 plt.plot(M[::skip], R[::skip])
 plt.plot(M[::skip], P[::skip])
@@ -69,10 +77,30 @@ plt.plot(M[::skip], L[::skip])
 plt.plot(M[::skip], T[::skip])
 plt.plot(M[::skip], eps[::skip])
 plt.plot(M[::skip], kap[::skip])
-#plt.plot(M[::skip], dm[::skip])
+plt.legend(["rho [rho0]", \
+            "R [R0]", "P [1e3 P0]", "L [L0]", "T [1e7 K]", \
+            "eps [W/kg]", "kappa [1e3 cm^2/g]"], \
+            loc="best")
+# ************************************************************ #
+plt.figure()
 
-plt.legend(["rho [rho0]", "R [R0]", "P [1e3 P0]", "L [L0]", "T [1e7 K]", \
-            "eps [W/kg]", "kappa 1e3 cm^2/g]"], loc="best")
+plt.grid('on')
+plt.xlabel("mass [M0]")
+
+plt.plot(M[::skip], dm[::skip], "b.")
+
+plt.legend(["dm [-kg]"])
+# ************************************************************ #
+plt.figure()
+
+plt.grid('on')
+plt.xlabel("mass [M0]")
+
+plt.plot(M[::skip], F_r[::skip])
+plt.plot(M[::skip], F_c[::skip])
+
+plt.legend(["F_rad", "F_con"])
+# ************************************************************ #
 
 plt.show()
 

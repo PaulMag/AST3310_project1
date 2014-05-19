4d96b9b3bdf30cb1f0ac78491ca59e534b674b7b
PaulMag <paulms@ulrik.uio.no>
2014-05-19 17:23:25 +0200

    Script for simulating many different R0 at once.

---
+++ run_many.py
@@ -0,0 +1,13 @@
+import sys, os
+
+R0_values = [0.4, 0.7, 1, 1.5, 2.5, 4, 7 ,  10, 15, 20, 25, 30, 35, 40]
+
+i = 0
+
+for R0 in R0_values:
+    print "%d / %d: Working on %g" % (i, len(R0_values), R0)
+    os.system("python solar_model.py %g r0_%g" % (R0, R0))
+    i += 1
+
+print "%d / %d: FINISHED!" % (i, len(R0_values))
+
